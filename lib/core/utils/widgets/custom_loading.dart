2a066ab35b51b8170a0228b37cc2277b7aa7bb5d
mohamed emad <muhamedemad.155@gmail.com>
2025-05-19 17:21:37 +0300

    change custom loading

--- lib/core/utils/widgets/custom_loading.dart
+++ lib/core/utils/widgets/custom_loading.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
-import '../constants/app_assets.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:melamine_elsherif/core/config/themes.dart/theme.dart';
 
 class CustomLoadingWidget extends StatelessWidget {
   final double? width;
   final double? height;
+  final Color? color;
 
   const CustomLoadingWidget({
     super.key,
     this.width,
     this.height,
+    this.color,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Lottie.asset(
-      AppAnimations.loading,
-      width: width ?? 80,
-      height: height ?? 80,
-      fit: BoxFit.contain,
+    return SpinKitPulse(
+      color: color ?? AppTheme.primaryColor,
+      size: width ?? 40.0, // Default size if width is not provided
     );
   }
 }
